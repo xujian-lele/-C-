8402f7f51fd966d228e455502e1b4eeec4aea5b6
xujian-lele <648909477@qq.com>
2014-12-08 17:35:11 +0800

    test for overload

--- extends/main.cpp
+++ extends/main.cpp
@@ -5,58 +5,16 @@
 #include "assert.h"
 
 using namespace std;
-class Father
+int f(int x)
 {
-public:
-    string testPublic()
-    {
-        return "This is testPublic";
-    };
-    int p1;
-protected:
-    string testProtected()
-    {
-        return "This is testProtect";
-    };
-    int p2;
-private:
-    string testPrivate()
-    {
-        return "This is testPrivate";
-    }
-    int p3;
-};
-
-class publicChild : public Father
-{
-public:
-    string test()
-    {
-        return testPublic()+" in public child's public";
-    };
-protected:
-    string test1()
-    {
-        return testProtected() + " in public child's protected";
-    };
-private:
-    string test2()
-    {
-        return " This is in public child's private";
-    }
-
-};
-void f(int x)
-{
-    //the value is passed by value
-    x = 100;
-    cout << "The inputed num of inner function is:" << x << endl;
+    cout << x << endl;
 }
-
+int f(double x)
+{
+    cout << x << endl;
+}
 int main()
 {
-    int a = 10;
-    f(a);
-    cout << "The inputed num of outer function f is:" << a << endl;
+
     return 0;
 }
