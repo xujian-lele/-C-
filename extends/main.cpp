50f2984f8debdc2b515601d104bee0ae84d2a1e8
xujian-lele <648909477@qq.com>
2014-12-08 15:46:01 +0800

    Test for:default parameter for a functioin

--- extends/main.cpp
+++ extends/main.cpp
@@ -5,95 +5,57 @@
 #include "assert.h"
 
 using namespace std;
-
-class A{
-public:
-    virtual void fun()= 0;
-};
-
-class B : public A
-{
-    public:
-    virtual void fun(){
-        cout << "This is B::fun()" << endl;
-    }
-};
-
-class C : public A
+class Father
 {
 public:
-        virtual void fun(){
-        cout << "This is C::fun()" << endl;
+    string testPublic()
+    {
+        return "This is testPublic";
+    };
+    int p1;
+protected:
+    string testProtected()
+    {
+        return "This is testProtect";
+    };
+    int p2;
+private:
+    string testPrivate()
+    {
+        return "This is testPrivate";
     }
+    int p3;
 };
 
-void Test(A *a)
+class publicChild : public Father
 {
-    assert(a == NULL);
-    a->fun();
-}
-
-class a{
 public:
-    a(){};
-    a(int n){
-        this->value = n;
+    string test()
+    {
+        return testPublic()+" in public child's public";
+    };
+protected:
+    string test1()
+    {
+        return testProtected() + " in public child's protected";
+    };
+private:
+    string test2()
+    {
+        return " This is in public child's private";
     }
-    int value;
+
 };
-//值传递
-void swap1(int x, int y){
-    int temp;
-    temp = x;
-    x = y;
-    y = temp;
-    cout << x <<"  "<< y <<endl;
-}
-//地址传递
-void swap2(int *x, int *y)
+
+int f(int x = 42);
+int f(int x)
 {
-    int temp;
-    temp = *x;
-    *x = *y;
-    *y = temp;
-    cout << *x <<"  "<< *y <<endl;
-}
-//引用传递
-void swap3(int &x, int &y)
-{
-    int temp;
-    temp = x;
-    x = y;
-    y = temp;
-    cout << x << "  " << y << endl;
+    cout << "The inputed num is:" << x << endl;
 }
 
 int main()
 {
-
-    int a = 6;
-    int* v1 = &a;
-    int b = 8;
-    int *v2 = NULL;
-    v2 = &b;
-    //*v2 = 8;
-    cout << v1 << " " << v2 << endl;
-    cout << *v1 << " " << *v2 << endl;
-    //swap1(*v1, *v2);
-    //swap2(v1,v2);
-    //swap2(&v1, &v2);
-    swap3(*v1, *v2);
-    cout << v1 << " " << v2 << endl;
-    cout << *v1 << " " << *v2 << endl;
-    int bb = 0123;
-    cout << bb << endl;
-    int cc = 0x123;
-    cout << cc << endl;
-    int dd = 123;
-    cout << dd << endl;
-    string input;
-    getline(cin,input);
-    cout << "U had input:" << endl;
-    cout << input << endl;
+    f();
+    f(28);
     return 0;
 }
