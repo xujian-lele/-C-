45e0dbb0ae201ccff2debac4a19036883bf61eb9
xujian-lele <648909477@qq.com>
2014-12-09 19:30:22 +0800

    Test thread:pass a parameter to the function

--- extends/main.cpp
+++ extends/main.cpp
@@ -2,16 +2,31 @@
 #include "iostream"
 #define PTHRED_NUM 5
 using namespace std;
-void* sayhello(void* args)
+class A
 {
-    cout << "Hello..." << endl;
-}
+    public:
+    static void* sayhello(void* args)
+    {
+        int i = *((int *)args);
+        cout << "Hello..i am:" << i << endl;
+    }
+};
 int main()
 {
     pthread_t tid[PTHRED_NUM];
+    int index[PTHRED_NUM];
     for(int i=0;i<PTHRED_NUM;i++)
     {
-        int ret = pthread_create(&tid[i], NULL, sayhello, NULL);
+        index[i] = i;
+        int ret = pthread_create(&tid[i], NULL, A::sayhello, (void*)&index[i]);
+        if(ret != 0)
+        {
+            cout << "pthread_create():error:" << ret << endl;
+        }
     }
-    pthread_exit(NULL);
+
+    for(int i=0; i<PTHRED_NUM; i++)
+    {
+        pthread_join(tid[i],NULL);
+    }
 }
