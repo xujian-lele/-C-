3a51f246644b593792718808230cd10bc64f107d
xujian-lele <648909477@qq.com>
2014-12-09 16:08:19 +0800

    test code:virtual method

--- extends/main.cpp
+++ extends/main.cpp
@@ -1,63 +1,46 @@
-#define NDEBUG
-#include <iostream>
-#include <Head.h>
-#include "Date.h"
-#include "assert.h"
-
+#include<iostream>
+#include "stdio.h"
 using namespace std;
-//定义类型名
-typedef unsigned int UNINT;
-//定义结构体，公用体，枚举类型的公用名
-typedef struct Node
+
+class A
 {
-    int a;
-    int b;
+public:
+    void foo()
+    {
+        printf("1\n");
+    }
+    virtual void fun()
+    {
+        printf("2\n");
+    }
 };
-//定义数组
-//int a[10]
-typedef int INT_ARRAY_10[10] ;
-typedef int INT_ARRAY_20[20] ;
-//定义指针
-typedef char * STRING;
-typedef int (*myfun) (int a,int b);
-int Max(int a, int b)
+class B : public A
 {
-    return a > b ? a : b;
-}
-int main()
+public:
+    void foo()
+    {
+        printf("3\n");
+    }
+    void fun()
+    {
+        printf("4\n");
+    }
+};
+int main(void)
 {
-    myfun FUN;
-    FUN = Max;
-    cout << FUN(1,2) <<endl;
-
-    STRING a = "i love C++";
-    cout << a << endl;
-
-    UNINT A;
-    A = 10;
-    cout << A << endl;
-
-    INT_ARRAY_10 B;
-    for(int i=0;i<10;i++){
-        B[i] = i;
-    }
-
-    for(int i=0;i<10;i++){
-       cout << B[i] << endl;
-    }
-
-    char *p1,*p2;
-    p1 = "I am p1";
-    p2 = "I am p2";
-
-    char* p3,p4;
-    p3 = "I am p3";
-    p4 = 'c';
-
-    STRING p5,p6;
-    p5 = "I am p5";
-    p6 = "I am p6";
-
-
+    A a;
+    B b;
+    A *p = &a;
+    p->foo();
+    p->fun();
+    p = &b;
+    p->foo();
+    p->fun();
+    B *pb = &b;
+    pb->foo();
+    pb->fun();
+    B *q = (B *)&a;
+    q->foo();
+    q->fun();
     return 0;
 }
