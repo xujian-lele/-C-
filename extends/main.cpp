28e53e503edc41b6b5e7aaa9498f24868f82eee8
xujian-lele <648909477@qq.com>
2014-12-09 23:52:42 +0800

    test for:pthread_cond_t

--- extends/main.cpp
+++ extends/main.cpp
@@ -1,51 +1,90 @@
-#include "pthread.h"
 #include "iostream"
-#define PTHRED_NUM 5
+#include "pthread.h"
+#define BOUND 5
 using namespace std;
-int sum;
-pthread_mutex_t sum_mutex;
-class A
+int task = 10;
+pthread_mutex_t task_mutex;
+pthread_cond_t task_cond;
+void* sayhello1(void* args)
 {
-    public:
-    static void* sayhello(void* args)
+    pthread_t pid = pthread_self();
+    cout << "[" << pid << "] hello in thread " <<  *( ( int* )args ) << endl;
+    bool is_signaled = false;
+    while(1)
     {
-        int i = *((int *)args);
-        cout << "Hello..i am:" << i << endl;
-        pthread_mutex_lock(&sum_mutex);
-        cout << "before + sum is: " << sum <<"in thread" << i << endl;
-        sum += i;
-        cout << "after + sum is: " << sum <<"in thread" << i << endl;
-        pthread_mutex_unlock(&sum_mutex);
-        pthread_exit(0);
+        pthread_mutex_lock(&task_mutex);
+        if(task > BOUND)
+        {
+            cout << "[" << pid << "] take task: " << task << " in thread " << *( (int*)args ) << endl;
+            task--;
+        }
+        else if(!is_signaled)
+        {
+            cout << "[" << pid << "] pthread_cond_signal in thread " << *( ( int* )args ) << endl;
+            pthread_cond_signal(&task_cond);
+        }
+        pthread_mutex_unlock(&task_mutex);
+        if(task == 0)
+        {
+            break;
+        }
     }
-};
+}
+
+void* sayhello2(void* args)
+{
+    pthread_t pid = pthread_self();
+    cout << "[" << pid << "] hello in thread " <<  *( ( int* )args ) << endl;
+    while(1)
+    {
+        pthread_mutex_lock(&task_mutex);
+        if(task > BOUND)
+        {
+            cout << "[" << pid << "] pthread_cond_signal in thread " << *( ( int* )args ) << endl;
+            pthread_cond_wait(&task_cond,&task_mutex);
+        }
+        else
+        {
+            cout << "[" << pid << "] take task: " << task << " in thread " << *( (int*)args ) << endl;
+            task--;
+        }
+        pthread_mutex_unlock(&task_mutex);
+        if(task == 0)
+        {
+            break;
+        }
+    }
+}
 int main()
 {
-    pthread_t tid[PTHRED_NUM];
-    int index[PTHRED_NUM];
+
+    pthread_mutex_init(&task_mutex,NULL);
+    pthread_cond_init(&task_cond,NULL);
+
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    pthread_mutex_init(&sum_mutex,NULL);
-    for(int i=0;i<PTHRED_NUM;i++)
+    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
+
+    pthread_t tid1;
+    int index1 = 1;
+    int res1 = pthread_create(&tid1,&attr,sayhello1,(void*) &index1);
+    if(res1 != 0)
     {
-        index[i] = i;
-        int ret = pthread_create(&tid[i],&attr, A::sayhello, (void*)&index[i]);
-        if(ret != 0)
-        {
-            cout << "pthread_create():error:" << ret << endl;
-        }
+        cout << "pthread_create error in creating" << index1 << " thread" <<endl;
     }
-pthread_attr_destroy(&attr);
-void* status;
-    for(int i=0; i<PTHRED_NUM; i++)
+
+    pthread_t tid2;
+    int index2 = 2;
+    int res2 = pthread_create(&tid2,&attr,sayhello2,(void*) &index2);
+    if(res2 != 0)
     {
-        int res = pthread_join(tid[i],&status);
-        if(res != 0)
-        {
-            cout << "pthread_join():failed" << tid[i] << endl;
-        }
+        cout << "pthread_create error in creating" << index2 << " thread" <<endl;
     }
-    pthread_mutex_destroy(&sum_mutex);
-    cout << "finally sum is: " << sum << endl;
+
+    pthread_join(tid1,NULL);
+    pthread_join(tid2,NULL);
+
+    pthread_mutex_destroy(&task_mutex);
+    pthread_cond_destroy(&task_cond);
+    return 0;
 }
