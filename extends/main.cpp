4c13a56bbd5d08216df8a3339451956e352cf6e7
xujian-lele <648909477@qq.com>
2014-12-09 19:57:31 +0800

    Test pthread_attr_t 's method

--- extends/main.cpp
+++ extends/main.cpp
@@ -9,24 +9,38 @@ class A
     {
         int i = *((int *)args);
         cout << "Hello..i am:" << i << endl;
+        void* status = (void*)(i + 10);
+        pthread_exit(status);
     }
 };
 int main()
 {
     pthread_t tid[PTHRED_NUM];
     int index[PTHRED_NUM];
+    pthread_attr_t attr;
+    pthread_attr_init(&attr);
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
     for(int i=0;i<PTHRED_NUM;i++)
     {
         index[i] = i;
-        int ret = pthread_create(&tid[i], NULL, A::sayhello, (void*)&index[i]);
+        int ret = pthread_create(&tid[i],&attr, A::sayhello, (void*)&index[i]);
         if(ret != 0)
         {
             cout << "pthread_create():error:" << ret << endl;
         }
     }
-
+pthread_attr_destroy(&attr);
+void* status;
     for(int i=0; i<PTHRED_NUM; i++)
     {
-        pthread_join(tid[i],NULL);
+        int res = pthread_join(tid[i],&status);
+        if(res != 0)
+        {
+            cout << "pthread_join():failed" << tid[i] << endl;
+        }
+        else
+        {
+            cout << "pthread_join():get status:" << (long)status << endl;
+        }
     }
 }
