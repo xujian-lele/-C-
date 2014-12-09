f0f7fbd941817f962be2d97dac324f2ae9790f4f
xujian-lele <648909477@qq.com>
2014-12-09 18:10:14 +0800

    my first thread program

--- extends/main.cpp
+++ extends/main.cpp
@@ -1,66 +1,17 @@
-#include<iostream>
+#include "pthread.h"
+#include "iostream"
+#define PTHRED_NUM 5
 using namespace std;
-
-class Base
+void* sayhello(void* args)
 {
-public:
-    virtual void f(float x)
-    {
-        cout<<"Base::f(float)"<< x <<endl;
-    }
-    void g(float x)
-    {
-        cout<<"Base::g(float)"<< x <<endl;
-    }
-    void h(float x)
-    {
-        cout<<"Base::h(float)"<< x <<endl;
-    }
-};
-class Derived : public Base
+    cout << "Hello..." << endl;
+}
+int main()
 {
-public:
-    virtual void f(float x)
+    pthread_t tid[PTHRED_NUM];
+    for(int i=0;i<PTHRED_NUM;i++)
     {
-        cout<<"Derived::f(float)"<< x <<endl;   //¶àÌ¬¡¢¸²¸Ç
+        int ret = pthread_create(&tid[i], NULL, sayhello, NULL);
     }
-    void g(int x)
-    {
-        cout<<"Derived::g(int)"<< x <<endl;     //Òþ²Ø
-    }
-    void h(float x)
-    {
-        cout<<"Derived::h(float)"<< x <<endl;   //Òþ²Ø
-    }
-};
-int main(void)
-{
-    Derived d;
-    Base *pb = &d;
-    Derived *pd = &d;
-    // Good : behavior depends solely on type of the object
-    pb->f(3.14f);   // Derived::f(float) 3.14
-    pd->f(3.14f);   // Derived::f(float) 3.14
-
-    // Bad : behavior depends on type of the pointer
-    pb->g(3.14f);   // Base::g(float)  3.14
-    pd->g(3.14f);   // Derived::g(int) 3
-
-    // Bad : behavior depends on type of the pointer
-    pb->h(3.14f);   // Base::h(float) 3.14
-    pd->h(3.14f);   // Derived::h(float) 3.14
-
-    Base e;
-    Base *p = &e;
-    Derived *q = (Derived *)&e;
-    //Good :
-    p->f(0.000001f);    //Base::f(float) 0.00000001
-    q->f(0.000001f);    //Base::f(float) 0.00000001
-
-    p->g(0.000001f);    //Base::f(float) 0.00000001
-    q->g(0.000001f);    //Derived::f(float) 0.00000001
-
-    p->h(0.000001f);    //Base::h(float) 0.000001f
-    q->h(0.000001f);    //Base::h(float) 0.000001f
-    return 0;
+    pthread_exit(NULL);
 }
