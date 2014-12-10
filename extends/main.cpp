3aa216507f804b1688c6afe08c2057440ff78876
xujian-lele <648909477@qq.com>
2014-12-10 11:19:19 +0800

    Test code muti-thread:producer and consumer

--- extends/main.cpp
+++ extends/main.cpp
@@ -1,90 +1,84 @@
 #include "iostream"
 #include "pthread.h"
-#define BOUND 5
 using namespace std;
-int task = 10;
-pthread_mutex_t task_mutex;
-pthread_cond_t task_cond;
-void* sayhello1(void* args)
-{
-    pthread_t pid = pthread_self();
-    cout << "[" << pid << "] hello in thread " <<  *( ( int* )args ) << endl;
-    bool is_signaled = false;
-    while(1)
-    {
-        pthread_mutex_lock(&task_mutex);
-        if(task > BOUND)
-        {
-            cout << "[" << pid << "] take task: " << task << " in thread " << *( (int*)args ) << endl;
-            task--;
-        }
-        else if(!is_signaled)
-        {
-            cout << "[" << pid << "] pthread_cond_signal in thread " << *( ( int* )args ) << endl;
-            pthread_cond_signal(&task_cond);
-        }
-        pthread_mutex_unlock(&task_mutex);
-        if(task == 0)
-        {
-            break;
-        }
-    }
-}
+int storage = 0;
+pthread_mutex_t storage_mutex;
+pthread_cond_t storage_empty;
+pthread_cond_t storage_full;
 
-void* sayhello2(void* args)
+void* produce(void* args)
 {
     pthread_t pid = pthread_self();
-    cout << "[" << pid << "] hello in thread " <<  *( ( int* )args ) << endl;
+    cout << "[" << pid << "]: in thread " << (*(int*)args) << endl;
     while(1)
     {
-        pthread_mutex_lock(&task_mutex);
-        if(task > BOUND)
+        pthread_mutex_lock(&storage_mutex);
+        if(storage >= 10)
         {
-            cout << "[" << pid << "] pthread_cond_signal in thread " << *( ( int* )args ) << endl;
-            pthread_cond_wait(&task_cond,&task_mutex);
+            pthread_cond_wait(&storage_full,&storage_mutex);
         }
         else
         {
-            cout << "[" << pid << "] take task: " << task << " in thread " << *( (int*)args ) << endl;
-            task--;
+            cout << "before produce storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
+            storage++;
+            cout << "after  produce storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
+            pthread_cond_signal(&storage_empty);
         }
-        pthread_mutex_unlock(&task_mutex);
-        if(task == 0)
+        pthread_mutex_unlock(&storage_mutex);
+    }
+}
+
+void* consume(void* args)
+{
+    pthread_t pid = pthread_self();
+    cout << "[" << pid << "]: in thread " << (*(int*)args) << endl;
+    while(1)
+    {
+        pthread_mutex_lock(&storage_mutex);
+        if(storage <= 0)
         {
-            break;
+            pthread_cond_wait(&storage_empty, &storage_mutex);
         }
+        else
+        {
+            cout << "before consume storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
+            storage--;
+            cout << "after  consume storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
+            pthread_cond_signal(&storage_full);
+        }
+        pthread_mutex_unlock(&storage_mutex);
     }
 }
 int main()
 {
+    pthread_mutex_init(&storage_mutex,NULL);
+    pthread_cond_init(&storage_empty,NULL);
+    pthread_cond_init(&storage_full,NULL);
 
-    pthread_mutex_init(&task_mutex,NULL);
-    pthread_cond_init(&task_cond,NULL);
-
+    pthread_t p1;
+    pthread_t c1;
     pthread_attr_t attr;
     pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
-
-    pthread_t tid1;
-    int index1 = 1;
-    int res1 = pthread_create(&tid1,&attr,sayhello1,(void*) &index1);
-    if(res1 != 0)
+    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+    //producer 1
+    int index_p1 = 1;
+    int res_p1 = pthread_create(&p1,&attr,produce,&index_p1);
+    if(res_p1 != 0)
     {
-        cout << "pthread_create error in creating" << index1 << " thread" <<endl;
+        cout << "Pthread_create failed:producer 1" << endl;
     }
-
-    pthread_t tid2;
-    int index2 = 2;
-    int res2 = pthread_create(&tid2,&attr,sayhello2,(void*) &index2);
-    if(res2 != 0)
+    //consumer 2
+    int index_c1 = -1;
+    int res_c1 = pthread_create(&c1,&attr,consume,&index_c1);
+    if(res_c1 != 0)
     {
-        cout << "pthread_create error in creating" << index2 << " thread" <<endl;
+        cout << "Pthread_create failed: consumer 1" << endl;
     }
+    pthread_attr_destroy(&attr);
+    pthread_join(p1,NULL);
+    pthread_join(c1,NULL);
 
-    pthread_join(tid1,NULL);
-    pthread_join(tid2,NULL);
-
-    pthread_mutex_destroy(&task_mutex);
-    pthread_cond_destroy(&task_cond);
-    return 0;
+    pthread_mutex_destroy(&storage_mutex);
+    pthread_cond_destroy(&storage_empty);
+    pthread_cond_destroy(&storage_full);
 }
