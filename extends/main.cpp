07f732571a40ec1b4aa84c572d6faf6019b865dd
xujian-lele <648909477@qq.com>
2014-12-10 22:08:40 +0800

    test for : stl

--- extends/main.cpp
+++ extends/main.cpp
@@ -1,84 +1,22 @@
 #include "iostream"
-#include "pthread.h"
+#include "vector"
+#include "algorithm"
+
 using namespace std;
-int storage = 0;
-pthread_mutex_t storage_mutex;
-pthread_cond_t storage_empty;
-pthread_cond_t storage_full;
-
-void* produce(void* args)
+int main(void)
 {
-    pthread_t pid = pthread_self();
-    cout << "[" << pid << "]: in thread " << (*(int*)args) << endl;
-    while(1)
+    //STL中的vector
+    vector<int> vector_int;
+    int element;
+    while(cin >> element)
     {
-        pthread_mutex_lock(&storage_mutex);
-        if(storage >= 10)
-        {
-            pthread_cond_wait(&storage_full,&storage_mutex);
-        }
-        else
-        {
-            cout << "before produce storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
-            storage++;
-            cout << "after  produce storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
-            pthread_cond_signal(&storage_empty);
-        }
-        pthread_mutex_unlock(&storage_mutex);
+        vector_int.push_back(element);
     }
-}
-
-void* consume(void* args)
-{
-    pthread_t pid = pthread_self();
-    cout << "[" << pid << "]: in thread " << (*(int*)args) << endl;
-    while(1)
+    //sort是STL中的算法
+    sort(vector_int.begin(), vector_int.end());
+    //vector能像数组那样进行访问
+    for(int i=0;i<vector_int.size();i++)
     {
-        pthread_mutex_lock(&storage_mutex);
-        if(storage <= 0)
-        {
-            pthread_cond_wait(&storage_empty, &storage_mutex);
-        }
-        else
-        {
-            cout << "before consume storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
-            storage--;
-            cout << "after  consume storage is: " << storage << "in [" << pid << "]: in thread " << (*(int*)args)<< endl;
-            pthread_cond_signal(&storage_full);
-        }
-        pthread_mutex_unlock(&storage_mutex);
+        cout << vector_int[i] << endl;
     }
-}
-int main()
-{
-    pthread_mutex_init(&storage_mutex,NULL);
-    pthread_cond_init(&storage_empty,NULL);
-    pthread_cond_init(&storage_full,NULL);
-
-    pthread_t p1;
-    pthread_t c1;
-    pthread_attr_t attr;
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
-    //producer 1
-    int index_p1 = 1;
-    int res_p1 = pthread_create(&p1,&attr,produce,&index_p1);
-    if(res_p1 != 0)
-    {
-        cout << "Pthread_create failed:producer 1" << endl;
-    }
-    //consumer 2
-    int index_c1 = -1;
-    int res_c1 = pthread_create(&c1,&attr,consume,&index_c1);
-    if(res_c1 != 0)
-    {
-        cout << "Pthread_create failed: consumer 1" << endl;
-    }
-    pthread_attr_destroy(&attr);
-    pthread_join(p1,NULL);
-    pthread_join(c1,NULL);
-
-    pthread_mutex_destroy(&storage_mutex);
-    pthread_cond_destroy(&storage_empty);
-    pthread_cond_destroy(&storage_full);
 }
