4cab7a8608a59c6f65b22864a9a26f2a3133387f
xujian-lele <648909477@qq.com>
2014-12-12 14:51:24 +0800

    test STL algorigthm : find() is avaliable for array

--- extends/main.cpp
+++ extends/main.cpp
@@ -1,43 +1,17 @@
 #include "iostream"
-#include "list"
 #include "algorithm"
 
 using namespace std;
-bool dayu100(int x){
-    bool b = (x > 100);
-    return b;
-}
-class a{
-public :
-    a(){
+#define SIZE  20
+int main(void){
+    int array[SIZE];
+    array[1] = 10000;
+    int *p = find(array,array+SIZE,10000);
+    if(p == array+SIZE){
+        cout << "Not find" << endl;
+    }else{
+        cout << "Has find" << endl;
     }
-    ~a(){
-    }
-    virtual void f(int x){};
-};
-typedef list<int> int_list;
-int main(void)
-{
-    int_list lst;
-    int_list lst1;
-    for(int i=10;i>=0;i--){
-        lst.push_back(i);
-    }
-    cout << lst.front() << endl;
-    cout << lst.back() << endl;
-
-    for(int i=100;i<105;i++){
-        lst1.push_back(i);
-    }
-    lst.merge(lst1);
-cout << lst.size() << endl;
-lst.remove_if(dayu100);
-cout << lst.size() << endl;
-cout << lst.back() << endl;
-
-a A;
-cout << sizeof(A) << endl;
-cout << sizeof(double) << endl;
-cout << sizeof(short *) << endl;
-
+    cout << *p << endl;
+    return 0;
 }
