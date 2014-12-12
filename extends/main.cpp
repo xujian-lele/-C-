d3ee0851044256ab11947e353b585aaab1c9ef02
xujian-lele <648909477@qq.com>
2014-12-12 10:51:59 +0800

    test for list

--- extends/main.cpp
+++ extends/main.cpp
@@ -1,45 +1,40 @@
 #include "iostream"
-#include "pthread.h"
-#include "vector"
+#include "list"
 #include "algorithm"
 
 using namespace std;
-
+bool dayu100(int x){
+    bool b = (x > 100);
+    return b;
+}
+class a{
+public :
+    a(){
+    }
+    ~a(){
+    }
+    virtual void f(int x){};
+};
+typedef list<int> int_list;
 int main(void)
 {
-    vector<int> vec;
-    vec.reserve(20);
-    int i = 0 ;
-    while(i <= 10)
-    {
-        vec.push_back(i);
-        i++;
+    int_list lst;
+    int_list lst1;
+    for(int i=10;i>=0;i--){
+        lst.push_back(i);
     }
+    cout << lst.front() << endl;
+    cout << lst.back() << endl;
 
-    cout << "vector's size : " << vec.size() << endl;//11
-    remove(vec.begin(),vec.end(),0);
-    cout << vec.size() << endl;//11,remove前后size没变，那么remove到底是啥意思？
-
-    cout << "vector's size : " << vec.size() << endl;
-    cout << "vector's capacity : " << vec.capacity() << endl;
-    cout << "vector's max_size : " << vec.max_size() << endl;
-
-    cout << "vector's front : " << vec.front() << endl;
-    cout << "vector's back : " << vec.back() << endl;
-
-    vector<int>::iterator itr = vec.begin();
-    while(true)
-    {
-        if(*itr == 1)
-        {
-           vec.erase(itr);
-           break;
-        }
-        itr++;
+    for(int i=100;i<105;i++){
+        lst1.push_back(i);
     }
+    lst.merge(lst1);
+cout << lst.size() << endl;
+lst.remove_if(dayu100);
+cout << lst.size() << endl;
+cout << lst.back() << endl;
 
-    cout << "vector's size : " << vec.size() << endl;
-    cout << "vector's front : " << vec.front() << endl;
-    swap(vec[0],vec[1]);
-    cout << "vector's front : " << vec.front() << endl;
+a A;
+cout << sizeof(A) << endl;
 }
